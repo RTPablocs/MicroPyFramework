fcf997098f290b51d6a326778d9c54ea69acbf3b
RTPablocs <conejerosorianopablo@gmail.com>
2021-04-22 00:05:13 +0200

    updated imports, added urlParse and changed return

--- app/httpServer.py
+++ app/httpServer.py
@@ -1,6 +1,7 @@
 from wsgiref.simple_server import make_server
 from wsgiref.util import setup_testing_defaults, request_uri
 from app.router import routingService
+from app.parseURL import urlParse
 from extensions.banner import bannerSpawn
 import os
 
@@ -11,21 +12,19 @@ class app:
         self.HOST = 'localhost'
         self.motd = bannerSpawn()
         self.router = routingService()
+        self.version = 'v0.1-rc2'
 
     def baseServe(self, environ, start_response):
-        path = str(request_uri(environ)).split('/')[-1]
-        
+        path = urlParse(str(request_uri(environ)))
         result = self.router.routerEval(path)
         status = result['code']
         headers = [('Content-Type', 'application/json; charset=utf-8')]
         
         start_response(status, headers)
         
-        ret = result['actionResult']
-        
-        return [ret.encode()]
+        return [str(result).encode()]
 
     def run(self):
         with make_server(self.HOST, self.PORT, self.baseServe) as httpd:
-            print(self.motd, '\n', 'Serving app on port:', self.PORT)
+            print(self.motd, '\n', 'Serving app on port:', self.PORT, '\n', self.version )
             httpd.serve_forever()
