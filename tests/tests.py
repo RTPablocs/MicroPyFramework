ca9159657860784868b3072cdda36a9222657400
RTPablocs <conejerosorianopablo@gmail.com>
2021-04-22 20:28:06 +0200

    Still testing, why do you keep looking?

--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,10 @@
 from app.routes import routes
 from app.greet import Greet
 import urllib.parse as url
-from app.router import routingService
+from app.router import RoutingProvider
 
+rs = RoutingProvider()
 
-
-rs = routingService()
-
-path = 'http://localhost:8051/hello/Pablo'
-result = rs.routerEval(path)
+path = 'http://localhost:8051/df'
+result = rs.router_evaluate(path)
 print(result)
-
