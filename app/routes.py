0521c87d9d6b05185589fc6cf806e32ba3431aed
RTPablocs <conejerosorianopablo@gmail.com>
2021-04-22 20:26:36 +0200

    Added /favicon.ico as solution to issue #1

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,8 @@ from app.greet import Greet
 routes = [
     {'path': '/hello', 'action': Greet},
     {'path': '/hello/{var}', 'action': Greet},
-    {'path': '/', 'action': Greet}
+    {'path': '/', 'action': Greet},
+    {'path': '/favicon.ico', 'action': None}
 ]
 
 
