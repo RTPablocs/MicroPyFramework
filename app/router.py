b2daa306b6e1f765aa1b8b4cb9be597ae2371722
RTPablocs <conejerosorianopablo@gmail.com>
2021-04-22 20:26:03 +0200

    Fixed issue #1, refactored and changed routing-evaluate conditions

--- app/router.py
+++ app/router.py
@@ -1,42 +1,40 @@
 import urllib.parse as url
 from app.routes import routes
 
-class routingService:
+
+class RoutingProvider:
 
     def __init__(self):
         self.routes = routes
-    
-    def dynamicEval(self, p):
+
+    def dynamic_evaluation(self, p):
         for r in self.routes:
 
             if '{var}' in r['path'] and p[-1] != '/':
 
-                evalPath = url.urlparse(p).path.split('/')
-                evalPath[-1] = '{var}'
-                convertedPath = '/'.join(evalPath)
-                
-                if cPath == r['path']: 
-                    dynamicArgument = url.urlparse(p).path.split('/')[-1]
-                    return {'path': convertedPath, 'realPath': p, 'dynamic': True, 'dynamicArgument': dynamicArgument}
+                eval_path = url.urlparse(p).path.split('/')
+                eval_path[-1] = '{var}'
+                converted_path = '/'.join(eval_path)
+
+                if converted_path == r['path']:
+                    dynamic_argument = url.urlparse(p).path.split('/')[-1]
+                    return {'path': converted_path, 'realPath': p, 'dynamic': True, 'dynamicArgument': dynamic_argument}
 
             elif r['path'] == p:
-                return  {'path': p, 'dynamic': False}
-                    
+                return {'path': p, 'dynamic': False}
 
-    def routerEval(self, route):
+    def router_evaluate(self, route):
 
-        resultDict = self.dynamicEval(route)
-        
-        
+        result_dict = self.dynamic_evaluation(route)
+
         for r in self.routes:
-            if resultDict and resultDict['dynamic'] == True: 
-                actionResult = r['action'](resultDict['dynamicArgument'])
-                return {'code': '200 OK', 'actionResult': resultDict}
-            
-            elif resultDict and resultDict['dynamic'] == False:
-                actionResult = r['action']()
-                return{'code': '200 OK', 'actionResult': actionResult}
+            if result_dict and result_dict['dynamic'] is True:
+                action_result = r['action'](result_dict['dynamicArgument'])
+                return {'code': '200 OK', 'actionResult': action_result}
 
-            elif resultDict and resultDict['path'] not in r['path'] : 
+            elif result_dict and result_dict['dynamic'] is False:
+                action_result = r['action']()
+                return {'code': '200 OK', 'actionResult': action_result}
+
+            else:
                 return {'code': '404 ERROR', 'actionResult': 'Path Not found'}
-            
