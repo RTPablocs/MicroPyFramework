149f778754d5cf7919b54d04a65f75dd940b62f7
RTPablocs <conejerosorianopablo@gmail.com>
2021-04-27 21:27:47 +0200

    Implementing method to routes

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,8 @@
 from app.greet import Greet
 
 routes = [
-    {'path': '/hello', 'action': Greet},
-    {'path': '/hello/{var}', 'action': Greet},
+    {'path': '/hello', 'method': 'GET', 'action': Greet},
+    {'path': '/hello/{var}', 'method': 'GET', 'action': Greet},
     {'path': '/', 'action': Greet},
     {'path': '/favicon.ico', 'action': None}
 ]
-
-
