3ff8dfdb4bec037ae3c639cdc47c99d7a8f984e1
RTPablocs <conejerosorianopablo@gmail.com>
2021-05-13 17:53:59 +0200

    Imported json and defined get_request_body(), also added kwargs to router_evaluate function

--- app/httpServer.py
+++ app/httpServer.py
@@ -1,7 +1,7 @@
+import json
 from socketserver import ThreadingMixIn
 from wsgiref.simple_server import make_server, WSGIServer
 from wsgiref.util import request_uri
-
 from app.router import RoutingProvider
 from extensions.utils import banner_generate
 from extensions.utils import url_parse
@@ -9,6 +9,13 @@ from extensions.utils import url_parse
 
 class MultiThreading(ThreadingMixIn, WSGIServer):
     daemon_threads = True
+
+
+def get_request_body(environ):
+    content_length = environ['CONTENT_LENGTH']
+    request_body = environ['wsgi.input'].read(int(content_length))
+    request_json = json.loads(request_body)
+    return request_json
 
 
 class App:
@@ -23,13 +30,14 @@ class App:
     def app_server(self, environ, start_response):
         path = url_parse(str(request_uri(environ)))
         method = environ['REQUEST_METHOD']
-        result = self.router.router_evaluate(path, method)
+        body = get_request_body(environ) if environ['CONTENT_LENGTH'] else None
+        result = self.router.router_evaluate(path=path, method=method, body=body)
         status = result['code']
         headers = [('Content-Type', 'application/json; charset=utf-8')]
 
         start_response(status, headers)
 
-        return [str(result).encode()]
+        return [json.dumps(result).encode('utf-8')]
 
     def run(self):
         with make_server(self.HOST, self.PORT, self.app_server, MultiThreading) as httpd:
